c204a7c5d4f6663025806ea10bd7bbe06fb46c3b
kiwi-fruitiwi <zane.tian@gmail.com>
2021-10-09 02:59:41 -0700

    encapsulate fourier series logic in methods for square, sawtooth

--- sketch.js
+++ sketch.js
@@ -18,8 +18,9 @@ coding plan:
 
  */
 let font
-let time
+let time = 0
 let wave = [] // stores points that we've drawn
+let slider
 
 function preload() {
     font = loadFont('fonts/Meiryo-01.ttf')
@@ -29,8 +30,43 @@ function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
 
-    time = 0
+    slider = createSlider(1, 10)
 }
+
+function getSawtoothValues(n) {
+
+}
+
+
+// wraps up the three values of a fourier series epicycle: x, y, and radius
+class fourierValues {
+    constructor(x, y, r) {
+        this.x = x
+        this.y = y
+        this.r = r
+    }
+}
+
+
+// gets the values of the ith square wave term in the fourier series
+// a square wave's series is: 4*sin(nθ)/(nπ) for odd natural numbers
+function getSquareWaveValues(i) {
+    let n = 2*i+1
+    let r = 75*(4/(n*PI))
+    return new fourierValues(r*cos(n*time), r*sin(n*time), r)
+}
+
+
+// a sawtooth wave's series is: 2sin(nθ)/(nπ); every odd n is negative
+function getSawtoothWaveValues(i) {
+    let n = i+1
+    if (n%2===1)
+        n *= -1
+
+    let r = 75*(2/(n*PI))
+    return new fourierValues(r*cos(n*time), r*sin(n*time), r)
+}
+
 
 function draw() {
     background(209, 80, 30)
@@ -41,34 +77,31 @@ function draw() {
 
     let x = 0
     let y = 0
-    let r = 75
-    let n = 0
 
-    let prevX, prevY
+    let prevPos
 
-    for (let i=0; i<5; i++) {
-        n = 2*i+1
-        prevX = x
-        prevY = y
+    for (let i=0; i<slider.value(); i++) {
+        prevPos = new p5.Vector(x, y)
+        let fourierValues = getSawtoothWaveValues(i)
+        x += fourierValues.x
+        y += fourierValues.y
 
-        r = 75*(4/(n*PI))
-        x += r*cos(n*time)
-        y += r*sin(n*time)
-
+        // connect the center of each epicycle to the next
         stroke(0, 0, 100)
-        line(prevX, prevY, x, y)
+        line(prevPos.x, prevPos.y, x, y)
 
+        // draw the new epicycle we created! :D
         stroke(0, 0, 100, 20)
-        circle(prevX, prevY, r*2)
+        circle(prevPos.x, prevPos.y, fourierValues.r*2)
     }
 
     wave.unshift(y)
-    let offset = 240
+    let offsetX = 240
 
     // we want to have some space between our drawer and the wave
-    translate(offset, 0)
-    line(x-offset, y, 0, wave[0])
-    strokeWeight(8)
+    translate(offsetX, 0)
+    line(x-offsetX, y, 0, wave[0])
+    strokeWeight(10)
     point(0, wave[0])
 
     // display all the wave coordinates we've saved
